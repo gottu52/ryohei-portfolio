663bf7c4a8bd6dd043d4760a9919d13a0a930bec
gottu52 <ryohei846@ezweb.jp>
2021-12-30 12:05:12 +0900

    .

--- src/components/organism/Works.jsx
+++ src/components/organism/Works.jsx
@@ -8,6 +8,7 @@ import RunningImg from "../../images/works/RunningMethodImage.png";
 import HairSalonImg from "../../images/works/HairSalonImage.png";
 import GymImg from "../../images/works/GymImage.png";
 import TodoImg from "../../images/works/TodoImage.png";
+import "../../css/mediaQuery.css";
 
 export const Works = () => {
 
@@ -52,40 +53,48 @@ export const Works = () => {
           slidesPerView={1}
           style={{height: "75%"}}
         >
-          <SwiperSlide>
-            <a href="https://gottu52.github.io/running-method/" target="_blank" rel="noopener noreferrer">
+          <SwiperSlide className='slide'>
+            <a className="workImg" href="https://gottu52.github.io/running-method/" target="_blank" rel="noopener noreferrer">
               <img src={RunningImg} alt="running" style={imgStyle}/>
             </a>
-            <p style={textStyle}>Running-method</p>
-            <p style={textStyle}>製作日数 : 4日</p>
-            <p style={textStyle}>言語 : HTML,CSS,JS</p>
+            <div className="workWrappr">
+              <p style={textStyle}>Running-method</p>
+              <p style={textStyle}>製作日数 : 4日</p>
+              <p style={textStyle}>言語 : HTML,CSS,JS</p>
+            </div>
           </SwiperSlide>
 
-          <SwiperSlide>
-            <a href="https://gottu52.github.io/hair-salon/" target="_blank" rel="noopener noreferrer">
+          <SwiperSlide className='slide'>
+            <a className="workImg" href="https://gottu52.github.io/hair-salon/" target="_blank" rel="noopener noreferrer">
               <img src={HairSalonImg} alt="HairSalon" style={imgStyle}/>
             </a>
-            <p style={textStyle}>Hair-salon</p>
-            <p style={textStyle}>製作日数 : 2日</p>
-            <p style={textStyle}>言語 : HTML,CSS,JS</p>
+            <div className="workWrappr">
+              <p style={textStyle}>Hair-salon</p>
+              <p style={textStyle}>製作日数 : 2日</p>
+              <p style={textStyle}>言語 : HTML,CSS,JS</p>
+            </div>
           </SwiperSlide>
 
-          <SwiperSlide>
-            <a href="https://gottu52.github.io/gold-gym/" target="_blank" rel="noopener noreferrer">
+          <SwiperSlide className='slide'>
+            <a className="workImg" href="https://gottu52.github.io/gold-gym/" target="_blank" rel="noopener noreferrer">
               <img src={GymImg} alt="gym" style={imgStyle}/>
             </a>
-            <p style={textStyle}>Gold-gym</p>
-            <p style={textStyle}>製作日数 : 3日</p>
-            <p style={textStyle}>言語 : HTML,CSS,JS</p>
+            <div className="workWrappr">
+              <p style={textStyle}>Gold-gym</p>
+              <p style={textStyle}>製作日数 : 3日</p>
+              <p style={textStyle}>言語 : HTML,CSS,JS</p>
+            </div>
           </SwiperSlide>
 
-          <SwiperSlide>
-            <a href="https://gottu52.github.io/Todo-App/" target="_blank" rel="noopener noreferrer">
+          <SwiperSlide className='slide'>
+            <a className="workImg" href="https://gottu52.github.io/Todo-App/" target="_blank" rel="noopener noreferrer">
               <img src={TodoImg} alt="todo" style={imgStyle}/>
             </a>
-            <p style={textStyle}>Todo-app</p>
-            <p style={textStyle}>製作日数 : 1日</p>
-            <p style={textStyle}>言語 : HTML,CSS,React</p>
+            <div className="workWrappr">
+              <p style={textStyle}>Todo-app</p>
+              <p style={textStyle}>製作日数 : 1日</p>
+              <p style={textStyle}>言語 : HTML,CSS,React</p>
+            </div>
           </SwiperSlide>
         </Swiper>
       </div>
